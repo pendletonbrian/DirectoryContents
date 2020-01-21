aad66717a08c9781882ffda1c73393efbdf838de
pennyrave <pendletonbrian@gmail.com>
2020-01-21 12:30:54 -0600

    start of GUI

--- DirectoryContents/DirectoryContents/ViewModels/MainWindowViewModel.cs
+++ DirectoryContents/DirectoryContents/ViewModels/MainWindowViewModel.cs
@@ -3,11 +3,65 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Windows.Input;
 using DirectoryContents.Classes;
 
 namespace DirectoryContents.ViewModels
 {
     public class MainWindowViewModel : NotifyObject
     {
+        #region Public Members
+
+        public static RoutedCommand BrowseCommand = new RoutedCommand();
+        public static RoutedCommand ParseCommand = new RoutedCommand();
+
+        #endregion Public Members
+
+        #region Private Members
+
+        private string m_DirectoryToParse = string.Empty;
+        private volatile bool m_IsParsing = false;
+
+        #endregion Private Members
+
+        #region Public Properties
+
+        public string DirectoryToParse
+        {
+            get { return m_DirectoryToParse; }
+
+            set
+            {
+                if (string.IsNullOrWhiteSpace(m_DirectoryToParse) ||
+                    m_DirectoryToParse.Equals(value, StringComparison.OrdinalIgnoreCase) == false)
+                {
+                    m_DirectoryToParse = value;
+
+                    RaisePropertyChanged(nameof(DirectoryToParse));
+                }
+            }
+        }
+
+        #endregion Public Properties
+
+        #region constructor
+
+        public MainWindowViewModel() { }
+
+        #endregion
+
+        #region Public Methods
+
+        internal bool CanGenerate()
+        {
+            return string.IsNullOrWhiteSpace(m_DirectoryToParse).Equals(false);
+        }
+
+        internal bool IsParsing()
+        {
+            return m_IsParsing;
+        }
+
+        #endregion Public Methods
     }
 }
--- DirectoryContents/DirectoryContents/Views/MainWindow.xaml.cs
+++ DirectoryContents/DirectoryContents/Views/MainWindow.xaml.cs
@@ -7,11 +7,13 @@ using System.Windows;
 using System.Windows.Controls;
 using System.Windows.Data;
 using System.Windows.Documents;
+using System.Windows.Forms;
 using System.Windows.Input;
 using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using DirectoryContents.ViewModels;
 
 namespace DirectoryContents
 {
@@ -20,9 +22,69 @@ namespace DirectoryContents
     /// </summary>
     public partial class MainWindow : Window
     {
+        #region Private Members
+
+        private MainWindowViewModel m_ViewModel;
+
+        #endregion
+
+        #region constructor
+
         public MainWindow()
         {
             InitializeComponent();
+
+            m_ViewModel = new MainWindowViewModel();
+            DataContext = m_ViewModel;
+        }
+
+        #endregion
+
+        private void BrowseCommand_CanExecute(object sender, CanExecuteRoutedEventArgs e)
+        {
+            if (m_ViewModel is null)
+            {
+                e.CanExecute = true;
+            }
+            else
+            {
+                e.CanExecute = m_ViewModel.IsParsing().Equals(false);
+            }
+
+            e.Handled = true;
+        }
+
+        private void BrowseCommand_Executed(object sender, ExecutedRoutedEventArgs e)
+        {
+            using (FolderBrowserDialog diag = new FolderBrowserDialog())
+            {
+                diag.SelectedPath = Environment.GetFolderPath(Environment.SpecialFolder.DesktopDirectory);
+                diag.Description = "Select the folder to parse.";
+
+                if (diag.ShowDialog() == System.Windows.Forms.DialogResult.OK)
+                {
+                    m_ViewModel.DirectoryToParse = diag.SelectedPath;
+                }
+            }
+        }
+
+        private void ParseCommand_CanExecute(object sender, CanExecuteRoutedEventArgs e)
+        {
+            if (m_ViewModel is null)
+            {
+                e.CanExecute = false;
+            }
+            else
+            {
+                e.CanExecute = m_ViewModel.CanGenerate();
+            }
+
+            e.Handled = true;
+        }
+
+        private void ParseCommand_Executed(object sender, ExecutedRoutedEventArgs e)
+        {
+
         }
     }
 }
