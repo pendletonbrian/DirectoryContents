fa1d13cc8c1e090da9d8457ee8b2a93db1a609dc
pennyrave <pendletonbrian@gmail.com>
2020-02-01 07:16:48 -0600

    Added last two types.

---
+++ DirectoryContents/DirectoryContents/Classes/FileHash/SHA384.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace DirectoryContents.Classes.FileHash
+{
+    public class SHA384 : IFileHash
+    {
+        /// <summary>
+        /// Get the hash for the SHA-384 algorithim.
+        /// </summary>
+        /// <param name="data">
+        /// </param>
+        /// <returns>
+        /// </returns>
+        public ulong GetHash(byte[] data)
+        {
+            byte[] hash = null;
+
+            using (System.Security.Cryptography.SHA384 crypto = System.Security.Cryptography.SHA384.Create())
+            {
+                hash = crypto.ComputeHash(data);
+            }
+
+            return BitConverter.ToUInt64(hash, 0);
+        }
+    }
+}
---
+++ DirectoryContents/DirectoryContents/Classes/FileHash/SHA512.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace DirectoryContents.Classes.FileHash
+{
+    public class SHA512 : IFileHash
+    {
+        /// <summary>
+        /// Get the hash for the SHA-512 algorithim.
+        /// </summary>
+        /// <param name="data">
+        /// </param>
+        /// <returns>
+        /// </returns>
+        public ulong GetHash(byte[] data)
+        {
+            byte[] hash = null;
+
+            using (System.Security.Cryptography.SHA512 crypto = System.Security.Cryptography.SHA512.Create())
+            {
+                hash = crypto.ComputeHash(data);
+            }
+
+            return BitConverter.ToUInt64(hash, 0);
+        }
+    }
+}
